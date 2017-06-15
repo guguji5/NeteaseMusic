d683f4dd5388b6694e994442add3c262f64dfe8f
wanghq11 <wanghq11@vanke.com>
2017-06-15 09:00:18 +0800

    filter

--- neteaseMusic/src/service/filters.js
+++ neteaseMusic/src/service/filters.js
@@ -38,26 +38,33 @@ export default{
   },
   MillisecondToDate: function MillisecondToDate(msd) {
     var time = parseFloat(msd) / 1000;
-    if (null != time && "" != time) {
-      if (time > 60 && time < 60 * 60) {
-        var m = parseInt(time / 60.0) > parseInt(9) ? parseInt(time / 60.0) : "0" + parseInt(time / 60.0);
-        var s = parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) > parseInt(9) ? parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) : "0" + parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60);
-        time = m + ":" + s;
-      } else if (time >= 60 * 60 && time < 60 * 60 * 24) {
-        var h = parseInt(time / 3600.0) > parseInt(9) ? parseInt(time / 3600.0) : "0" + parseInt(time / 3600.0)
-        var m = parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) > parseInt(9) ? parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) : "0" + parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60);
-        var s = parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
-          parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) > parseInt(9) ? parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
-          parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) : "0" + parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
-            parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60)
-        time = h + ":" + m + ":" + s;
-      } else {
-        time = "00" + parseInt(time) > parseInt(9) ? parseInt(time) : "0" + parseInt(time);
-      }
-    } else {
-      time = "00:00";
-    }
-    return time;
+    var min = Math.floor(time / 60);
+    var sec = Math.ceil(time % 60);
+
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
+
+    return min + ':' + sec;
+    // if (null != time && "" != time) {
+    //   if (time > 60 && time < 60 * 60) {
+    //     var m = parseInt(time / 60.0) > parseInt(9) ? parseInt(time / 60.0) : "0" + parseInt(time / 60.0);
+    //     var s = parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) > parseInt(9) ? parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60) : "0" + parseInt((parseFloat(time / 60.0) - parseInt(time / 60.0)) * 60);
+    //     time = m + ":" + s;
+    //   } else if (time >= 60 * 60 && time < 60 * 60 * 24) {
+    //     var h = parseInt(time / 3600.0) > parseInt(9) ? parseInt(time / 3600.0) : "0" + parseInt(time / 3600.0)
+    //     var m = parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) > parseInt(9) ? parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) : "0" + parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60);
+    //     var s = parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+    //       parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) > parseInt(9) ? parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+    //       parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60) : "0" + parseInt((parseFloat((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60) -
+    //         parseInt((parseFloat(time / 3600.0) - parseInt(time / 3600.0)) * 60)) * 60)
+    //     time = h + ":" + m + ":" + s;
+    //   } else {
+    //     time = "00" + parseInt(time) > parseInt(9) ? parseInt(time) : "0" + parseInt(time);
+    //   }
+    // } else {
+    //   time = "00:00";
+    // }
+    // return time;
 
   }
 }
